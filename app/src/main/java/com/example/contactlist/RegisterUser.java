aed2f8277599bd3ef6d8fee364b02f0531eb554e
DJ <j.devansh29@gmail.com>
2021-06-22 11:38:48 -0400

    Add register view

--- app/src/main/java/com/example/contactlist/RegisterUser.java
+++ app/src/main/java/com/example/contactlist/RegisterUser.java
@@ -1,6 +1,19 @@
 package com.example.contactlist;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterUser extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+    }
+
 }
