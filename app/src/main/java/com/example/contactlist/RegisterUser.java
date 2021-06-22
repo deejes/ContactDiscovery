f70c2500200b9e175be418a1d1bc613f951d9b2b
DJ <j.devansh29@gmail.com>
2021-06-22 13:16:20 -0400

    Add firebase registration call

--- app/src/main/java/com/example/contactlist/RegisterUser.java
+++ app/src/main/java/com/example/contactlist/RegisterUser.java
@@ -5,15 +5,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterUser extends Activity {
+    private FirebaseAuth mAuth;
+    private EditText usernameInput, passwordInput;
+    private Button completeRegButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        mAuth = FirebaseAuth.getInstance();
 
+        completeRegButton = (Button) findViewById(R.id.complete_registration);
+
+        completeRegButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usernameInput = (EditText) findViewById(R.id.email_input);
+                passwordInput = (EditText) findViewById(R.id.password_input);
+
+                String username = usernameInput.getText().toString().trim();
+                String password = passwordInput.getText().toString().trim();
+                RegisterUserInFirebase(username,password);
+            }
+
+            private void RegisterUserInFirebase(String username, String password) {
+                mAuth.createUserWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            ;
+                        }
+                    }
+                });
+            }
+        });
     }
+
+
+
+
 
 }
